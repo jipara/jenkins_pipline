2545bccc2bfc797cd511881af04f8761126989bd
jipara <zhypargul.esengulova@gmail.com>
2019-08-24 11:33:56 -0500

    add

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,4 +8,13 @@ Prod:3.15.200.213''', name: 'Remote_instances', trim: false)])])
     stage("Install Apache"){
         sh "ssh ec2-user@${Remote_instances}  sudo yum install httpd -y"
     }
+    stage("Create Index.html"){
+        sh "scp index.html ec2-user@${Remote_instances}:/tmp"
+    }
+    stage("Move Files"){
+        sh "ssh ec2-user@${Remote_instances}  sudo mv /tmp/index.html /var/www/html/index.html"
+    }
+    stage("Restart httpd"){
+        sh "shh ec2-user@${Remote_instances"} sudo systemctl restart httpd"
+    }
 }
