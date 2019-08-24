e0a003a8a8705906b3781c8138a57fbe0bc7519b
jipara <zhypargul.esengulova@gmail.com>
2019-08-24 11:12:56 -0500

    add git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,5 @@
 node { 
     stage("say hello"){
-       properties([parameters([string(defaultValue: 'Jipara', description: 'past your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
-        sh "echo hello ${NAME}"
+       git https://github.com/jipara/jenkins-april.git
     }
 }
