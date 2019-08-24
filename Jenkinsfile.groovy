7037f21721d8f6957b4676328dfebc534215a9fb
jipara <zhypargul.esengulova@gmail.com>
2019-08-24 11:06:48 -0500

    add name inside code

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node { 
     stage("say hello"){
-       properties([pipelineTriggers([cron('''* * * * *''')])])
+       properties([parameters([string(defaultValue: 'Jipara', description: 'past your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * *')])])
         sh "echo hello ${NAME}"
     }
 }
