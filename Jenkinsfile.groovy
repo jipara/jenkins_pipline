379e34bb852b089f8df3388c3bcafa7b8558f671
jipara <zhypargul.esengulova@gmail.com>
2019-08-24 11:28:29 -0500

    add user

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,11 @@
 node { 
-    stage("say hello"){
+    stage("pull repo"){
        properties([parameters([string(defaultValue: '52.14.177.159', description: '''Dev:52.14.177.159
 QA:18.188.242.117
 Prod:3.15.200.213''', name: 'Remote_instances', trim: false)])])
        git 'https://github.com/jipara/jenkins-april.git'
+    }
+    stage("Install Apache"){
+        sh "ssh ec2-user@${Remote_instances}  sudo yum install httpd -y"
     }
 }
