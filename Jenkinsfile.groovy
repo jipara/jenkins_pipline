77dc3617446bbebde4f5ca5044d3759199dd6fce
jipara <zhypargul.esengulova@gmail.com>
2019-08-24 11:24:25 -0500

    add dev

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,8 @@
 node { 
     stage("say hello"){
+       properties([parameters([string(defaultValue: '52.14.177.159', description: '''Dev:52.14.177.159
+QA:18.188.242.117
+Prod:3.15.200.213''', name: 'Remote_instances', trim: false)])])
        git 'https://github.com/jipara/jenkins-april.git'
     }
 }
